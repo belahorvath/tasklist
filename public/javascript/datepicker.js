8c4007f7b07d2d37738aead8011e57ccf66bc725
kohlema5 <kohlema5@students.zhaw.ch>
2018-10-10 09:14:35 +0200

    responsive

--- public/javascript/datepicker.js
+++ public/javascript/datepicker.js
@@ -1,7 +1,8 @@
 function date(){
+    
     $('#datepicker').datepicker({
       inline: true,
       showOtherMonths: true,
       dayNamesMin: ['So','Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-    });
-};
+    }).focus();
+}

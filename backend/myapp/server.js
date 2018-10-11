c9b30fd2f8bb0191b1c99d5b353e881fe0f2a629
horvabel <horvabel@students.zhaw.ch>
2018-10-11 11:53:09 +0200

    Server mit css/javascript

--- backend/myapp/server.js
+++ backend/myapp/server.js
@@ -5,6 +5,8 @@ const express = require('express'),
       app = express();
         var path = require('path')
 
+
+app.use(express.static(__dirname + '/../../public'));
 app.use(morgan('dev'));
 app.use(express.static('../../index.html'));
 // parse application/x-www-form-urlencoded
@@ -27,6 +29,19 @@ app.post('/todos', function(req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/../../index.html'));
 });
+app.get('/public/css/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../../public/css/style.css'));
+});
+app.get('/public/css/datepicker.css', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../../public/css/datepicker.css'));
+});
+app.get('/backend/myapp/server.js', function (req, res) {
+  res.sendFile(path.join(__dirname + '/server.js'));
+});
+app.get('/public/javascript/datepicker.js', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../../public/javascript/datepicker.js'));
+});
+
 
 app.listen(8080, function(){
   console.log("ready captain.");

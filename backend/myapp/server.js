e1c8096ad93df7d82194df8b483174a08e7dc6f9
horvabel <horvabel@students.zhaw.ch>
2018-10-11 12:06:27 +0200

    Server mit css/javascript

--- backend/myapp/server.js
+++ backend/myapp/server.js
@@ -6,40 +6,27 @@ const express = require('express'),
         var path = require('path')
 
 
-app.use(express.static(__dirname + '/../../public'));
+app.use(express.static(path.join(__dirname, '/../../public')));
 app.use(morgan('dev'));
-app.use(express.static('../../index.html'));
+app.use(express.static(path.join(__dirname,'../../index.html')));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var todos = [];
-
-app.post('/todos', function(req, res) {
-  todos.push(req.body.todo);
-
-  res.format({
-    'application/json': function() {
-      res.status(200).json({'todo': req.body.todo});
-    },
-    'text/html': function() {
-      res.redirect(301, '/');
-    }});
-});
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/../../index.html'));
+  res.sendFile(path.join(__dirname, '/../../index.html'));
 });
 app.get('/public/css/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname + '/../../public/css/style.css'));
+  res.sendFile(path.join(__dirname, '/../../public/css/style.css'));
 });
 app.get('/public/css/datepicker.css', function (req, res) {
-  res.sendFile(path.join(__dirname + '/../../public/css/datepicker.css'));
+  res.sendFile(path.join(__dirname, '/../../public/css/datepicker.css'));
 });
 app.get('/backend/myapp/server.js', function (req, res) {
-  res.sendFile(path.join(__dirname + '/server.js'));
+  res.sendFile(path.join(__dirname, '/server.js'));
 });
 app.get('/public/javascript/datepicker.js', function (req, res) {
-  res.sendFile(path.join(__dirname + '/../../public/javascript/datepicker.js'));
+  res.sendFile(path.join(__dirname, '/../../public/javascript/datepicker.js'));
 });
 
 

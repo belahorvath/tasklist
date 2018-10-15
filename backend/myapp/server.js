9b26a80b78c7c7bf7df4b5b86548ad277a46ecd9
luchsan1 <luchsan1@students.zhaw.ch>
2018-10-15 16:26:18 +0200

    removed margan

--- backend/myapp/server.js
+++ backend/myapp/server.js
@@ -1,13 +1,11 @@
 "use strict";
 const express = require('express'),
       bodyParser = require('body-parser'),
-      morgan = require('morgan'),
       app = express();
         var path = require('path')
 
 
 app.use(express.static(__dirname + '/../../public'));
-app.use(morgan('dev'));
 app.use(express.static('../../index.html'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -41,7 +39,6 @@ app.get('/backend/myapp/server.js', function (req, res) {
 app.get('/public/javascript/datepicker.js', function (req, res) {
   res.sendFile(path.join(__dirname + '/../../public/javascript/datepicker.js'));
 });
-
 
 app.listen(8080, function(){
   console.log("ready captain.");

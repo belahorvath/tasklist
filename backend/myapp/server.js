cb7e719b8eebfc81069ab19f71585bb387912e83
Bela Horvath <horvabel@students.zhaw.ch>
2018-10-17 08:14:25 +0200

    final web3 milestone 1

--- backend/myapp/server.js
+++ backend/myapp/server.js
@@ -4,31 +4,13 @@ const express = require('express'),
       app = express();
         var path = require('path')
 
-
-app.use(express.static(__dirname + '/../../public'));
-app.use(express.static('../../index.html'));
-
-app.use(express.static(path.join(__dirname,'../../index.html')));
-app.use('/public', express.static(path.join(__dirname,'/../../public')));
+app.use(express.static(path.join(__dirname,'/../../public')));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/../../index.html'));
-});
-
-app.get('/public/css/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname + '/../../public/css/style.css'));
-});
-app.get('/public/css/datepicker.css', function (req, res) {
-  res.sendFile(path.join(__dirname + '/../../public/css/datepicker.css'));
-});
-app.get('/backend/myapp/server.js', function (req, res) {
-  res.sendFile(path.join(__dirname + '/server.js'));
-});
-app.get('/public/javascript/datepicker.js', function (req, res) {
-  res.sendFile(path.join(__dirname + '/../../public/javascript/datepicker.js'));
 });
 
 app.listen(8080, function(){

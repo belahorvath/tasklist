f8588898e2e1b1a499624ee9f4b3b33a343e4665
kohlema5 <kohlema5@students.zhaw.ch>
2018-11-12 16:05:25 +0100

    eee

---
+++ public/javascript/issueCollection.js
@@ -0,0 +1,27 @@
+class issueCollection {
+
+    constructor(tag) {
+        this.collection = [];
+        if(tag) {
+            this.riotjs_tag = tag;
+        }
+    }
+
+    save(){
+      localStorage.setItem("issueCollection", JSON.stringify(this.collection));
+    }
+
+    all() {
+        return this.collection;
+    }
+
+    add(model) {
+        this.collection.push(model);
+        this.save();
+        this.riotjs_tag.update();
+    }
+
+    fetch(){
+      this.collection = JSON.parse(localStorage.getItem("issueCollection")) || [];
+    }
+}

e7e9ef88e5d690a0ea28decce42db70e8abceb48
horvabel <horvabel@students.zhaw.ch>
2018-12-05 16:59:53 +0100

    Get funktioniert, post hat fehler 400 (bad formatting?)

--- server.js
+++ server.js
@@ -5,24 +5,35 @@ const express = require('express'),
         var path = require('path')
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var request = require('request');
 
-app.get('/api/projects', function(req,res) {
-  //modify the url in any way you want
-  var newurl = 'http://zhaw-issue-tracker-api.herokuapp.com/api/projects';
-  request(newurl).pipe(res);
+app.get('/api/projects/', function(req,res) {
+
+  var newurl = 'http://zhaw-issue-tracker-api.herokuapp.com/api/projects/' + req.query.id;
+
+  request.get(newurl, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body);
+
+    //Send data from get request as response back.
+    res.send(body);
+  });
+
 });
 
 app.post('/api/projects', function(req,res) {
-  //modify the url in any way you want
+
   var newurl = 'http://zhaw-issue-tracker-api.herokuapp.com/api/projects';
-  
+console.log(req.body);
   request.post(newurl, function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log(res.body);
   });
 
 });

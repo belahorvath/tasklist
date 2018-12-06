bd24e58d1b88c42daf8972177034e6a373892ee9
Bela Horvath <horvabel@students.zhaw.ch>
2018-12-06 10:48:34 +0100

    Create project and create issue works

--- public/javascript/datepicker.js
+++
@@ -1,8 +0,0 @@
-function date(){
-
-    $('#datepicker').datepicker({
-      inline: true,
-      showOtherMonths: true,
-      dayNamesMin: ['So','Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-    }).focus();
-}
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+//PROJECTS
 app.get('/api/projects/', function(req,res) {
 
   var newurl = 'http://zhaw-issue-tracker-api.herokuapp.com/api/projects/' + req.query.id;
@@ -31,6 +32,7 @@ var newurl = 'http://zhaw-issue-tracker-api.herokuapp.com/api/projects';
 var headers = {
   "Content-Type" : 'application/json'
 }
+console.log(req.body);
 
   request.post({headers: headers, url: newurl, json : req.body}, function (error, response, body) {
     if(response.statusCode != 200){
@@ -38,6 +40,29 @@ var headers = {
       console.log('statusCode:', response && response.statusCode);
     }
     res.send(body);
+  });
+
+});
+
+//ISSUES
+app.post('/api/projects/project_id/issues', function(req,res) {
+
+var newurl = 'http://zhaw-issue-tracker-api.herokuapp.com/api/projects/'+ req.body.project_id +'/issues';
+var headers = {
+  "Content-Type" : 'application/json'
+}
+console.log(req.body);
+  request.post({headers: headers, url: newurl, json : req.body}, function (error, response, body) {
+/*
+    if(response.statusCode != 200){
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);
+    }
+    */
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body', body);
+    //res.send(body);
   });
 
 });

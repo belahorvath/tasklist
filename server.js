c56932fc81f5602582dfc53a550d820e041698b8
Bela Horvath <horvabel@students.zhaw.ch>
2018-12-06 09:20:39 +0100

    post on server working with proxy

--- server.js
+++ server.js
@@ -2,38 +2,42 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
       app = express();
-        var path = require('path')
+var path = require('path')
+var request = require('request');
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var request = require('request');
 
 app.get('/api/projects/', function(req,res) {
 
   var newurl = 'http://zhaw-issue-tracker-api.herokuapp.com/api/projects/' + req.query.id;
 
   request.get(newurl, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body);
-
-    //Send data from get request as response back.
+    if(response.statusCode != 200){
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);
+    }
     res.send(body);
+
   });
 
 });
 
 app.post('/api/projects', function(req,res) {
 
-  var newurl = 'http://zhaw-issue-tracker-api.herokuapp.com/api/projects';
-console.log(req.body);
-  request.post(newurl, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-    console.log(res.body);
+var newurl = 'http://zhaw-issue-tracker-api.herokuapp.com/api/projects';
+var headers = {
+  "Content-Type" : 'application/json'
+}
+
+  request.post({headers: headers, url: newurl, json : req.body}, function (error, response, body) {
+    if(response.statusCode != 200){
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);
+    }
+    res.send(body);
   });
 
 });
